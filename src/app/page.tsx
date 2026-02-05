65e25e1fa4cf690ff766d7fbb65b976cb9de96da
Im-gowtham-cd <gowthamcdstudies@gmail.com>
2026-02-05 19:42:10 +0530

    HeroSection --Context

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,7 +14,20 @@ export default function App() {
                 <a href="">Download <i className="bx bx-arrow-to-bottom" /></a>
             </nav>
             <section className={HomeStyle.Home}>
+                <div className={HomeStyle.HomeContext}>
+                    <p>We Farm , We Evolve</p>
+                    <p>VFarm makes farming simple with easy tools and instant access to schemes, weather, and expert advice.
+                        Fast, secure, and built to support every farmer’s success.</p>
+                    <ul>
+                        <a href="">Explore <i className="bx bx-arrow-in-up-right-circle" /> </a>
+                    </ul>
+                </div>
+                <div className={HomeStyle.HomeDownload}>
+                    
+                </div>
+                <div className={HomeStyle.HomeImage}>
 
+                </div>
             </section>
         </div>
     )
